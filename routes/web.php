f24b5a1fc0db7ef1952bedfbcd934ed5daa0abf6
Andrii <andristwan28@gmail.com>
2024-08-11 00:58:56 +0700

    implementasi tailwind ui

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', ["name" => "Elaina"]);
+});
+
+Route::get('/blog', function(){
+    return view('blog');
+});
+
+Route::get('/contact', function(){
+    return view('contact', ["name" => "Elaina"]);
+});
+
+//buat 2 route
+// 1. /blog yang berisi 2 buah artike, judul dan isi
+//2. /contact isi email / sosmed
